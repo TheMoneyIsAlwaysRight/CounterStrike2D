137e941065b7c5627f0eb5ecfe83ef541e58d594
PAKJUNGMIN <vhzptvhdhfl@gmail.com>
2024-08-28 11:02:21 +0900

    Refacter : Grid 객체를 싱글턴으로 변경 및, Grid Class 레이블 정리

--- Assets/Script/AI/PathFind/Grid.cs
+++ Assets/Script/AI/PathFind/Grid.cs
@@ -1,18 +1,31 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-    Node[,] grid;
+    static Grid instance;
+
+    public static Grid Inst { get { return instance; } }
+
+    [Header("이동 불가능 지역의 레이어 마스크")]
     public LayerMask unwalkableMask; //지나갈 수 없는 지역을 나타내는 레이어마스크
+    [Header("격자의 크기 : 맵을 덮도록 설정")]
     public Vector2 gridWorldSize;//격자의 전체 사이즈
+    [Header("정점의 반지름")]
     public float nodeRadius; //노드의 반지름
+    [HideInInspector]
+    public int gridSizeX, gridSizeY; //격자의 밑변과 높이 크기
+
     float nodeDiameter; // 노드의 지름(격자 한칸의 변의 길이를 설정해줄 크기)
-    int gridSizeX, gridSizeY; //격자의 밑변과 높이 크기
+
+    public Node[,] grid;
     public List<Node> path;
     Vector3 worldBottomLeft;
 
+    private void Awake()
+    {
+        instance = this;
+    }
 
     private void Start()
     {
@@ -22,22 +35,23 @@ public class Grid : MonoBehaviour
         CreateGrid();
     }
 
+    //Method : 맵을 기반으로 Grid 생성 
     void CreateGrid()
     {
         grid = new Node[gridSizeX, gridSizeY]; //start에서 계산한 크기만큼 노드 2차원 배열 생성
         worldBottomLeft = transform.position - Vector3.right * (gridWorldSize.x / 2) - Vector3.up * (gridWorldSize.y / 2); //격자에서 왼쪽 아래 꼭지점
-        // Debug.Log($"worldBottomLeft = {worldBottomLeft}");
         for (int x = 0; x < gridSizeX; x++)
         {
             for (int y = 0; y < gridSizeY; y++)
             {
                 Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.up * (y * nodeDiameter + nodeRadius);
                 bool walkable = !(Physics2D.OverlapCircle(worldPoint, nodeRadius, unwalkableMask));
-                grid[x,y] =new Node(walkable, worldPoint, x, y);
-             
+                grid[x, y] = new Node(walkable, worldPoint, x, y);
+
             }
         }
     }
+
     public Node NodeFromWorldPoint(Vector3 worldPosition) //현재 캐릭터가 서 있는 노드가 어딘지 반환.
     {
 
@@ -49,56 +63,58 @@ public class Grid : MonoBehaviour
 
         int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
         int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+
         return grid[x, y];
     }
 
-    //private void OnDrawGizmos()
+    //public List<Node> GetNeighbors(Node node)
     //{
-    //    Gizmos.color = Color.blue;
-    //    Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, gridWorldSize.y, 0));
-
-    //    if (grid != null)
+    //    List<Node> neighbors = new List<Node>();
+    //    for (int x = -1; x <= 1; x++)
     //    {
-    //        foreach (Node n in grid)
+    //        for (int y = -1; y <= 1; y++)
     //        {
-    //            Gizmos.color = (n.walkable) ? Color.white : Color.red;
-    //            if (path != null)
+    //            if (x == 0 && y == 0) //자기 자신 제외.
     //            {
-    //                if (path.Contains(n))
-    //                {
-    //                    Gizmos.color = Color.black;
-    //                }
+    //                continue;
     //            }
-    //            else
-    //            {
-    //            }
-    //            Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
 
+    //            int checkX = node.gridX + x;
+    //            int checkY = node.gridY + y;
+
+    //            if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
+    //            {
+    //                neighbors.Add(grid[checkX, checkY]);
+    //            }
     //        }
     //    }
+    //    return neighbors;
     //}
 
-    public List<Node> GetNeighbors(Node node)
+    private void OnDrawGizmos()
     {
-        List<Node> neighbors = new List<Node>();
-        for (int x = -1; x <= 1; x++)
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, gridWorldSize.y, 0));
+
+        if (grid != null)
         {
-            for (int y = -1; y <= 1; y++)
+            foreach (Node n in grid)
             {
-                if (x == 0 && y == 0) //자기 자신 제외.
+                Gizmos.color = (n.walkable) ? Color.white : Color.red;
+                if (path != null)
                 {
-                    continue;
+                    if (path.Contains(n))
+                    {
+                        Gizmos.color = Color.black;
+                    }
                 }
-
-                int checkX = node.gridX + x;
-                int checkY = node.gridY + y;
-
-                if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
+                else
                 {
-                    neighbors.Add(grid[checkX, checkY]);
                 }
+                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
+
             }
         }
-        return neighbors;
     }
+
 }
--- Assets/Script/AI/PathFind/PathFinding.cs
+++ Assets/Script/AI/PathFind/PathFinding.cs
@@ -7,15 +7,12 @@ using UnityEngine;
 
 public class PathFinding : MonoBehaviour
 {
-    Grid grid;
-    private void Awake()
-    {
-        grid = GetComponent<Grid>();
-    }
+
+    //Method : A* 알고리즘 이용한 경로 찾기
     public List<Node> FindPath(Vector3 startPos, Vector3 targetPos)
     {
-        Node startNode = grid.NodeFromWorldPoint(startPos);
-        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+        Node startNode = Grid.Inst.NodeFromWorldPoint(startPos);
+        Node targetNode = Grid.Inst.NodeFromWorldPoint(targetPos);
         List<Node> openSet = new List<Node>(); //방문해야할 노드들
         List<Node> visited = new List<Node>(); //방문한 노드들
         openSet.Add(startNode);
@@ -48,12 +45,12 @@ public class PathFinding : MonoBehaviour
                 }
                 path.Reverse();
 
-                grid.path = path;
+                Grid.Inst.path = path;
 
                 return path;
 
             }
-            foreach (Node neighbour in grid.GetNeighbors(node))
+            foreach (Node neighbour in GetNeighbors(node))
             {
                 if (!neighbour.walkable || visited.Contains(neighbour))
                 {
@@ -61,13 +58,13 @@ public class PathFinding : MonoBehaviour
                     continue;
                 }
 
-                int newCostToNeighbor = node.gCost + GetDistance(node,neighbour);
+                int newCostToNeighbor = node.gCost + Heuristic(node,neighbour);
 
                 if (newCostToNeighbor < neighbour.gCost || !openSet.Contains(neighbour))
                 { 
 
                     neighbour.gCost = newCostToNeighbor;
-                    neighbour.hCost = GetDistance(neighbour, targetNode);
+                    neighbour.hCost = Heuristic(neighbour, targetNode);
                     neighbour.parent = node;
                    if(!openSet.Contains(neighbour))
                     {
@@ -80,8 +77,8 @@ public class PathFinding : MonoBehaviour
         return null;
 
     }
-
-    int GetDistance(Node A, Node B) 
+    //Method : A* 알고리즘 휴리스틱 함수
+    int Heuristic(Node A, Node B) 
     {
         int dstX = Mathf.Abs(A.gridX - B.gridX);
         int dstY = Mathf.Abs(A.gridY - B.gridY);
@@ -90,5 +87,33 @@ public class PathFinding : MonoBehaviour
             return 14 * dstY + 10 * (dstX - dstY);
         }
         return 14 * dstX + 10 * (dstY - dstX);
+    }
+
+    public List<Node> GetNeighbors(Node node)
+    {
+        int gridSizeX = Grid.Inst.gridSizeX;
+        int gridSizeY = Grid.Inst.gridSizeY;
+        Node[,] grid = Grid.Inst.grid;
+
+        List<Node> neighbors = new List<Node>();
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                if (x == 0 && y == 0) //자기 자신 제외.
+                {
+                    continue;
+                }
+
+                int checkX = node.gridX + x;
+                int checkY = node.gridY + y;
+
+                if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
+                {
+                    neighbors.Add(grid[checkX, checkY]);
+                }
+            }
+        }
+        return neighbors;
     }
 }
